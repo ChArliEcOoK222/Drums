fd8c9d6681aedd615acc52927e82d2b73b2bd177
Charlie C <charlie2007.cook@gmail.com>
2026-01-25 22:30:05 +0000

    Download songs from YouTube and stem the songs into components

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
-# How do I download a song as a .wav file?
 # How do I access the drums sound from a .wav file?
 # How do I put this sound into a format which I can read?
 
 # Required libraries
-from pytube import YouTube
+import subprocess # Run external programs
+from audio_separator.separator import Separator # Separate various instruments
 
-# Replace with your YouTube video URL
-url = "https://www.youtube.com/watch?v=HO0svGjVEP8&list=RDHO0svGjVEP8&start_radio=1"
+# Input the URL
+url = input("Enter the URL: ")
+# Initialise the Separator class
+separator = Separator()
+# Loading a drums model
+separator.load_model(model_filename="htdemucs_ft.yaml")
 
-try:
-    yt = YouTube(url)
-    # Get the highest quality audio stream
-    audio_stream = yt.streams.filter(only_audio=True).first()
-    
-    # Download it
-    audio_stream.download(output_path="downloads", filename="my_audio.mp3")
-    print("Download complete!")
-except Exception as e:
-    print("Error:", e)
+# Execute a command
+subprocess.run([
+    "yt-dlp",
+    "-x", # Exact audio
+    "--audio-format", "wav",
+    "-o", "/Users/charlie/Documents/Drums/%(title)s.%(ext)s", # Output template of FileName.Format
+    url 
+])
+
+# Performing the separation on the file
+file = separator.separate('Jeff Buckley - Forget Her (Official Video).wav')
